97a053a48e87ef14addda78734e3b5ed47b55f4f
Harsh-U-Joshi <48823979+Harsh-U-Joshi@users.noreply.github.com>
2026-02-19 13:00:20 +0530

    Create sample.cs

---
+++ sample.cs
@@ -0,0 +1,40 @@
+public static string GeneratePocoFromDataTable(DataTable table, string className)
+{
+    var sb = new StringBuilder();
+
+    sb.AppendLine($"public class {className}");
+    sb.AppendLine("{");
+
+    foreach (DataColumn column in table.Columns)
+    {
+        string columnName = column.ColumnName;
+        string typeName = GetCSharpType(column.DataType, column.AllowDBNull);
+
+        sb.AppendLine($"    public {typeName} {columnName} {{ get; set; }}");
+    }
+
+    sb.AppendLine("}");
+
+    return sb.ToString();
+}
+
+private static string GetCSharpType(Type type, bool isNullable)
+{
+    var typeName = type switch
+    {
+        Type t when t == typeof(int) => "int",
+        Type t when t == typeof(long) => "long",
+        Type t when t == typeof(string) => "string",
+        Type t when t == typeof(DateTime) => "DateTime",
+        Type t when t == typeof(bool) => "bool",
+        Type t when t == typeof(decimal) => "decimal",
+        Type t when t == typeof(double) => "double",
+        Type t when t == typeof(Guid) => "Guid",
+        _ => type.Name
+    };
+
+    if (isNullable && type.IsValueType)
+        typeName += "?";
+
+    return typeName;
+}
